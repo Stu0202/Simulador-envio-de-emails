f95b501cbc8135a9d1642d33fdbab685cc55250e
Jorge <jorgereyeslira02@gmail.com>
2024-09-19 21:40:36 -0500

    Mostrando las alertas junto a sus campos

--- js/app.js
+++ js/app.js
@@ -12,16 +12,16 @@ document.addEventListener('DOMContentLoaded',function(){
     
     function validar(e) {
        if (e.target.value.trim() === '') {
-            mostrarAlerta()
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`,e.target.parentElement)
        }
     }
 
 
-    function mostrarAlerta() {
+    function mostrarAlerta(mensaje,referencia) {
         const error = document.createElement('P')
-        error.textContent = 'Todos los campos son obligatorios'
+        error.textContent = mensaje
         error.classList.add('bg-red-600','text-white','p-2','text-center')
-        formulario.appendChild(error)
+        referencia.appendChild(error)
 
     }
 })
