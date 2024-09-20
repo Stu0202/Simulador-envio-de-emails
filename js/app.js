776cb3d9165342f70276c5ba273f9fd9fd62fee1
Jorge <jorgereyeslira02@gmail.com>
2024-09-19 21:25:32 -0500

    Añadiendo la alerta en el HTML

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded',function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario')
 
     inputEmail.addEventListener('blur', validar)
 
@@ -11,7 +12,16 @@ document.addEventListener('DOMContentLoaded',function(){
     
     function validar(e) {
        if (e.target.value.trim() === '') {
-        
+            mostrarAlerta()
        }
+    }
+
+
+    function mostrarAlerta() {
+        const error = document.createElement('P')
+        error.textContent = 'Todos los campos son obligatorios'
+        error.classList.add('bg-red-600','text-white','p-2','text-center')
+        formulario.appendChild(error)
+
     }
 })
